2e4e0e9ee6c0a2c80cf70a1703382de85c3348f5
Alejandro-Meneses <alejandro214003@gmail.com>
2025-05-15 12:54:17 +0200

    usecallback

--- src/Components/Game/Board.js
+++ src/Components/Game/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useCallback } from "react";
 import "../../Styles/Board.css";
 import { useNavigate } from "react-router-dom"; // Importar para la navegación
 
